7679d52f3de58a04c20042ff307022cad5d6a4b2
Israel Flores <israelflo92@gmail.com>
2022-04-06 00:21:23 -0700

    problem one

---
+++ app.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+function myFunction(a, b) {
+  return a + b;
+}
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
